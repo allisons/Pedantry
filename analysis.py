d120f1cc0b692311feee26bf33609828a337b31b
allisons <allison.sliter@gmail.com>
2016-06-08 10:21:43 -0700

    Major refactoring

--- analysis.py
+++ analysis.py
@@ -7,68 +7,15 @@ from multiprocessing import Pool
 from sys import argv
 import os
 import logging
+logging.basicConfig()
 logger = logging.getLogger()
+handler = logging.StreamHandler()
 formatter = logging.Formatter(
         '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.setLevel(logging.DEBUG)
 
-def mapper_callhome(x):
-    return bootstrap(erpaprobs_noFW['callhome'], ["callhome"], 100, all=False)
 
-def mapper_TAL(x):
-    return bootstrap(erpaprobs_noFW['TAL'], ["TAL"], 100, all=False)
-
-def mapper_WSJ(x):
-    return bootstrap(erpaprobs_noFW['WSJ'], ["WSJ"], 100, all=False)
-
-def mapper_all(x):
-    return bootstrap(erpaprobs, ["callhome", "TAL", "WSJ"], 100)
-        
-def bootstrap(erpa_data, cols, sample_size, all=True):
-    subjects = erpa_data.groupby('id')
-    placeholder = []
-    for name, group in subjects:
-        dx = np.random.choice(group['dx'])
-        idx = np.random.choice(group.index, size = sample_size)
-        selection = group.loc[idx,:]
-        if all:
-            subset = "_all"
-        else:
-            subset = "_noFW"
-        values = {}
-        for col in cols:
-            values[col+subset] = np.mean(selection[col])-np.mean(selection["child"])
-        values['dx'] = dx
-        assert isinstance(dx, str)
-        values['id'] = name
-        assert isinstance(values['id'], str)
-        values = Series(values)
-        placeholder.append(values)
-    subjectstats = DataFrame(placeholder)
-    dxgroups = subjectstats.groupby("dx")
-    values = {}
-    for name, group in dxgroups:
-        for col in cols:
-            values[col+"_"+name] = np.mean(group[col+subset])
-    return Series(values)
-
-
-def corpus_comparison_viz():
-    fig, ax = plt.subplots(figsize=(15, 18), nrows=3, ncols=1, sharex=True)
-    ax[0].scatter(np.log(probs['child'], np.log(probs['callhome']), color='orchid')
-    ax[1].scatter(np.log(probs['child']), np.log(probs['TAL']), color='black')
-    ax[2].scatter(np.log(probs['child']), np.log(probs['WSJ']), color='blue')
-    fig.suptitle("Unigram Probabilities in Adult Corpus as a function of Child Corpus")
-    ax[0].set_ylabel("CallHome")
-    ax[0].set_ylim(-16,0)
-    ax[1].set_ylabel("This American Life")
-    ax[1].set_ylim(-16,0)
-    ax[2].set_ylim(-16,0)
-    ax[2].set_ylabel("Wall Street Journal")
-    ax[2].set_xlabel("OGIKids + CHILDES")
-    fig.savefig("unigram_probabs.png")
-    
 def define_FW(probs, cols):
     for col in cols:
         probs[col+"FW"] = probs[col] > np.percentile(probs[col], 99)
@@ -93,9 +40,8 @@ def fetch_probs(probs, erpa, tokens=False):
     
 def FW_remove(data, col_1, col_2):
     fw_groups = data.groupby([col_1, col_2])
-    assert len(fw_groups.get_group((True, True))) > 0
-    return pd.concat([fw_groups.get_group((False,False)),fw_groups.get_group((False,True)), 
-                      fw_groups.get_group((True,False))])    
+    return pd.concat([fw_groups.get_group((False,False)),fw_groups.get_group((False,True)),
+                      fw_groups.get_group((True,False))])
 
 
 def euclideanfit(x,y):
@@ -104,7 +50,8 @@ def euclideanfit(x,y):
     a = eigvec[2,1]/eigec[1,2]
     b = np.mean(y) - a*np.mean(x)
     return a, b
-
+    
+    
 def subject_summary(data, other):
     placeholder = []
     subjects = data.groupby("id")
@@ -119,23 +66,58 @@ def subject_summary(data, other):
         values['dx'] = dx
         values = Series(values)
         placeholder.append(values)
-    return DataFrame(placeholder)
+    return DataFrame(placeholder)    
+
+def run_bootstrap(N,n, erpaprobs):
+    def mapper_callhome(x):
+        return bootstrap(erpaprobs_noFW['callhome'], ["callhome"], 100, all=False)
+    def mapper_TAL(x):
+        return bootstrap(erpaprobs_noFW['TAL'], ["TAL"], 100, all=False)
+
+    def mapper_WSJ(x):
+        return bootstrap(erpaprobs_noFW['WSJ'], ["WSJ"], 100, all=False)
+
+    def mapper_all(x):
+        return bootstrap(erpaprobs, ["callhome", "TAL", "WSJ"], 100)       
+    def bootstrap(erpa_data, cols, sample_size, all=True):
+        subjects = erpa_data.groupby('id')
+        placeholder = []
+        for name, group in subjects:
+            dx = np.random.choice(group['dx'])
+            idx = np.random.choice(group.index, size = sample_size)
+            selection = group.loc[idx,:]
+            if all:
+                subset = "_all"
+            else:
+                subset = "_noFW"
+            values = {}
+            for col in cols:
+                values[col+subset] = np.mean(selection[col])-np.mean(selection["child"])
+            values['dx'] = dx
+            assert isinstance(dx, str)
+            values['id'] = name
+            assert isinstance(values['id'], str)
+            values = Series(values)
+            placeholder.append(values)
+        subjectstats = DataFrame(placeholder)
+        dxgroups = subjectstats.groupby("dx")
+        values = {}
+        for name, group in dxgroups:
+            for col in cols:
+                values[col+"_"+name] = np.mean(group[col+subset])
+        return Series(values)
     
-#Load ADOS data
-def loaddata(f):
-    return pd.read_csv(f)
-
-#Load Language Model data
-def loadLMs(f):
-    return pd.read_csv(f)
-
-def run_bootstrap(N,n):
+    mappermap = {'callhome':mapper_callhome,'TAL':mapper_TAL, 'WSJ':mapper_WSJ}
     pool = Pool(processes=N)
+    erpaprobs_noFW = {col : FW_remove(erpaprobs, 'childFW', col+"FW") for col in corpora}
+    logger.debug("Beginning full language model set bootstrap")
     outcomes_all = DataFrame(pool.map(mapper_all, xrange(n)))
     outcomes_all.to_csv("outputfiles/bootstrap_all_words_n="+str(n)+"_"+model_descrip+".csv")
+    logger.debug("Full language model bootstrap complete")
     outcomes_FW = {k:DataFrame(pool.map(v, xrange(n))) for k, v in mappermap]}
     for k, v in outcomes_FW.items():
         vfn = "outputfiles/bootstrap_no_fw_"+k+"_n="+str(n)+"_"+model_descrip+".csv"
+        logger.debug("beginning "+k+" function words removed bootstrap")
         v.to_csv(vfn, index=False)
         if not os.path.exists(vfn):
             logger.debug("Failed to save"+vfn)
@@ -143,25 +125,19 @@ def run_bootstrap(N,n):
             logger.debug(vfn+" successfully saved")
         
 
-if __name__ == "__main__":
-    model_descrip = argv[4]
-    logger.debug("Beginning "+model_descrip+"iteration of model")
-    erpa = loaddata(argv[1])
-    probs = loadLMs(argv[2])
+def create_erpaprobs(args):
+    erpa = pd.read_csv(args[0])
+    probs = pd.read_csv(args[1])
     corpora = ['callhome', 'TAL', 'WSJ']
     #Tokens or types?
-    tokens = bool(int(argv[3]))
+    tokens = bool(int(args[2]))
     
     if not os.path.isdir("outputfiles"):
         os.mkdir("outputfiles")
 
     #Mark out which words are in the 99th percentile for frequency
     probs = define_FW(probs,corpora)
-
-    #Map a corpora to its particular mapper for
-    mappermap = {'callhome':mapper_callhome,'TAL':mapper_TAL, 'WSJ':mapper_WSJ}
-
-    #Create a table that has each data point and its probability values
+    probs.to_csv("outfiles/probs_defined_FW.csv", index=False)
     erpaprobs = fetch_probs(probs, erpa, tokens=tokens)
     erpaprobsfn = "outputfiles/ERPA-stats_"+model_descrip+".csv"
     erpaprobs.to_csv(erpaprobsfn, index=False)
@@ -170,17 +146,20 @@ if __name__ == "__main__":
     else:
         logger.debug("Saved Erpa matrix")
     
+def run_bootstraps(args):
+    erpaprobs = pd.read_csv(args[0])    
+    run_bootstrap(100,10000, erpaprobs)
 
-    #Create a table that removes the function words as defined.
-    erpaprobs_noFW = {col : FW_remove(erpaprobs, 'childFW', col+"FW") for col in corpora}
-
-    #Save all those guys
-    for k, v in erpaprobs_noFW:
-        vfn = "outputfiles/ERPA-stats-noFW-"+k+"_"+model_descrip+".csv"
-        v.to_csv(vfn, index=False)
-        if not os.path.exists(vfn):
-            logger.debug("Failed to save"+vfn)
-        else:
-            logger.debug(vfn+" successfully saved")
-
-    run_bootstrap(100,10000)
+if __name__ == "__main__":
+    model_descrip = argv[1]
+    logger.debug("Beginning "+model_descrip+"iteration of model")
+    if argv[2] == "data":
+        create_erpaprobs(argv[3:])
+    if argv[2] == "bootstrap":
+        run_bootstraps(argv[3:])
+    if argv[2] = "test":
+        logger.debug("This test was successful")
+    else:
+        print "Terminating after doing basically nothing, so oops"
+    
+    
